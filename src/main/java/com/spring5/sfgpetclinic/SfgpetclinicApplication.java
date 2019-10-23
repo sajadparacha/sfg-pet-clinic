be0640466a6d765cffe8cce8e1e4d86eeced1d4c
sajadparacha <sajadparacha@gmail.com>
2019-10-24 01:23:39 +0300

    Commit initial project setup

---
+++ src/main/java/com/spring5/sfgpetclinic/SfgpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgpetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring5/sfgpetclinic/SfgpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgpetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
