0059bb6950394e9704f88c0f8595baeba300d83e
sajadparacha <sajadparacha@gmail.com>
2019-11-08 09:42:41 +0300

    An important note about spring configuration options. /**  * @author sajjad  * @SpringBootApplication , will tell spring to look for all the spring annoted classes  * e.g @Controller, @Componenet , @Service etc inside the packe where this annotation is . i.e in our case  * inside com.spring5.sfgpetclinic.web .  * Any Controller outside this package will not be scanned and included by the Spring Boot application.  * Let's say we have another package com.spring5.sfgpetclinic.web1 , any Controller inside it will not be picked hence if used anywhere will result in errors.  */

--- pet-clinic-web/src/main/java/com/spring5/sfgpetclinic/web/SfgpetclinicApplication.java
+++ pet-clinic-web/src/main/java/com/spring5/sfgpetclinic/web/SfgpetclinicApplication.java
@@ -3,6 +3,14 @@ package com.spring5.sfgpetclinic.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author sajjad
+ * @SpringBootApplication , will tell spring to look for all the spring annoted classes
+ * e.g @Controller, @Componenet , @Service etc inside the packe where this annotation is . i.e in our case
+ * inside com.spring5.sfgpetclinic.web .
+ * Any Controller outside this package will not be scanned and included by the Spring Boot application.
+ * Let's say we have another package com.spring5.sfgpetclinic.web1 , any Controller inside it will not be picked hence if used anywhere will result in errors.
+ */
 @SpringBootApplication
 public class SfgpetclinicApplication {
     /**
