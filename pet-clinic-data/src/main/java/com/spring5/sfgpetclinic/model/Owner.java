ba822797f95561fe6d5438815efea0b6a40aff4c
sajadparacha <sajadparacha@gmail.com>
2019-12-19 11:26:05 +0300

    Write Spring MVC test for owner controller.Closes #54

--- pet-clinic-data/src/main/java/com/spring5/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/spring5/sfgpetclinic/model/Owner.java
@@ -11,14 +11,26 @@ import java.util.Set;
 @EqualsAndHashCode(exclude = {"pets"})
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+//@Builder
 /*Lombok Annotations*/
 
 
 @Entity
 @Table(name = "owner")
 public class Owner extends Person{
-
+    /**
+     * Had to annotate the constructor with @Builder but normally we annotate class with @Builder
+     * This is because this object is having multiple enharitance and id was not being picked in the generated builder
+     * when we were annotating the class with @Builder , however annotating the constructor resolved the problem
+     * @param id
+     * @param firstName
+     * @param lastName
+     * @param pets
+     * @param address
+     * @param city
+     * @param telephone
+     */
+    @Builder
     public Owner(Long id, String firstName, String lastName, Set<Pet> pets, String address, String city, String telephone) {
         super(id, firstName, lastName);
         this.pets = pets;
